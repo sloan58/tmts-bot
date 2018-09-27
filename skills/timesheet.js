1a029c87d45d62a6e3c26b34df97c375f934ed93
Martin Sloan <martin.sloan@karma-tek.com>
2018-09-27 06:57:26 -0400

    building add time skill

--- skills/timesheet.js
+++ skills/timesheet.js
@@ -5,10 +5,38 @@ module.exports = function(controller) {
     controller.hears(['^add time'], 'direct_message', function(bot, message) {
 
         bot.createConversation(message, function(err, convo) {
-            if (!err) {
-                convo.say(`Okay, I will add time for you ${message.user.firstName}`);
-                convo.activate();
-            }
+            
+            // Create a yes/no question in the default thread...
+            convo.ask(`Okay, I will add time for you ${message.user.firstName}!\n\n` + 
+                      `How many hours would you like to add?`, [
+                {
+                    pattern:  '.*',
+                    callback: function(response, convo) {
+                        let hours = response.text
+                        hours = hours.replace(/\D/g,'')
+                        console.log(hours)
+                        console.log(isNaN(hours))
+                        if (isNaN(hours)) {
+                            convo.repeat()
+                        }
+                        convo.gotoThread('yes_thread')
+                    },
+                },
+            ]);
+
+            convo.activate();
+
+            // capture the results of the conversation and see what happened...
+            convo.on('end', function(convo) {
+
+                if (convo.successful()) {
+                    // this still works to send individual replies...
+                    bot.reply(message, 'Seeya nex time!');
+
+                }
+
+            });
+            
         });
 
     });
